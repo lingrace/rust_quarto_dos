ca7667aa2f743e78995458c2f3a4a0f77435c819
Grace Lin <lingrace@users.noreply.github.com>
2025-10-12 16:00:47 -0400

    board board: type alias for Board

--- src/main.rs
+++ src/main.rs
@@ -6,8 +6,9 @@
  
 const BOARD_SIZE: usize = 4;
 
+type Board = [[i32; BOARD_SIZE]; BOARD_SIZE];
 
-fn print_board(board: [[i32; BOARD_SIZE]; BOARD_SIZE]) {
+fn print_board(board: Board) {
     for row in board {
         let mut row_str: String = "".to_string();
         for col in row {
@@ -17,7 +18,7 @@ fn print_board(board: [[i32; BOARD_SIZE]; BOARD_SIZE]) {
     }
 }
 fn main() {
-    println!("pet pet pet");
-    let board = [[-1; BOARD_SIZE]; BOARD_SIZE]; // TODO: should not initialize to -1
+    println!("board board");
+    let board: Board = [[-1; BOARD_SIZE]; BOARD_SIZE]; // TODO: should not initialize to -1
     print_board(board);
 }
