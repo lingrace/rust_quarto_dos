76e718d3c38935678af0e1737924d15b943ffa2e
Grace Lin <lingrace@users.noreply.github.com>
2025-10-12 17:07:55 -0400

    -_-: formatting grr

--- src/main.rs
+++ src/main.rs
@@ -10,11 +10,15 @@ const BOARD_SIZE: usize = 4;
 type BoardCells = [[i32; BOARD_SIZE]; BOARD_SIZE];
 struct Board {
     cells: BoardCells,
+    cell_width: usize,
 }
 
 impl Board {
     fn new(cells: BoardCells) -> Self {
-        Board { cells }
+        Board {
+            cells,
+            ..Default::default()
+        }
     }
 }
 
@@ -22,30 +26,34 @@ impl Default for Board {
     fn default() -> Self {
         Board {
             cells: [[-1; BOARD_SIZE]; BOARD_SIZE],
+            cell_width: 4,
         }
     }
 }
 
 impl Display for Board {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        let sep = "─".repeat(self.cell_width * BOARD_SIZE + 3 * (BOARD_SIZE - 1) + 2);
         let board_str = self
             .cells
             .iter()
             .map(|row| {
-                row.iter()
-                    .map(|x| x.to_string())
+                let row_str = row
+                    .iter()
+                    .map(|x| format!("{:width$}", x, width = self.cell_width))
                     .collect::<Vec<_>>()
-                    .join(" | ")
+                    .join(" │ ");
+                format!("│{}│", row_str)
             })
             .collect::<Vec<_>>()
-            .join("\n");
+            .join(&format!("\n{}\n", sep));
         // TODO add line separator
 
-        return write!(f, "{}", board_str);
+        return write!(f, "{}\n{}\n{}", sep, board_str, sep);
     }
 }
 fn main() {
-    println!("^_^");
+    println!("-_-");
     let board = Board::default();
     let interesting_board = Board::new([[0, 1, 2, 3], [4, 14, 6, 7], [0, 1, 2, 3], [4, 5, 6, 15]]);
     println!("{}", board);
