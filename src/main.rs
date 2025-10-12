ef0912d60d322d41ac7690c074cbfa4dd3c9287d
Grace Lin <lingrace@users.noreply.github.com>
2025-10-12 16:12:13 -0400

    aiya: board becomes tiny struct

--- src/main.rs
+++ src/main.rs
@@ -6,10 +6,21 @@
  
 const BOARD_SIZE: usize = 4;
 
-type Board = [[i32; BOARD_SIZE]; BOARD_SIZE];
+
+struct Board {
+    cells: [[i32; BOARD_SIZE]; BOARD_SIZE]
+}
+
+impl Board {
+    fn new() -> Self {
+        Board {
+            cells:  [[-1; BOARD_SIZE]; BOARD_SIZE]
+        }
+    }
+}
 
 fn print_board(board: &Board) {
-    for row in board {
+    for row in board.cells {
         let mut row_str: String = "".to_string();
         for col in row {
             row_str += col.to_string().as_str();
@@ -19,8 +30,7 @@ fn print_board(board: &Board) {
 }
 
 fn main() {
-    println!("and board");
-    let board: Board = [[-1; BOARD_SIZE]; BOARD_SIZE]; // TODO: should not initialize to -1
-    print_board(&board);
+    println!("aiya");
+    let board = Board::new();
     print_board(&board);
 }
