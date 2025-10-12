77e629a344d3929d72efefa9d86107b402475643
Grace Lin <lingrace@users.noreply.github.com>
2025-10-12 16:52:57 -0400

    ^_^: moving new -> default, implementing new with inputs

--- src/main.rs
+++ src/main.rs
@@ -7,12 +7,19 @@ use std::fmt::Display;
 
 const BOARD_SIZE: usize = 4;
 
+type BoardCells = [[i32; BOARD_SIZE]; BOARD_SIZE];
 struct Board {
-    cells: [[i32; BOARD_SIZE]; BOARD_SIZE],
+    cells: BoardCells,
 }
 
 impl Board {
-    fn new() -> Self {
+    fn new(cells: BoardCells) -> Self {
+        Board { cells }
+    }
+}
+
+impl Default for Board {
+    fn default() -> Self {
         Board {
             cells: [[-1; BOARD_SIZE]; BOARD_SIZE],
         }
@@ -39,6 +46,8 @@ impl Display for Board {
 }
 fn main() {
     println!("^_^");
-    let board = Board::new();
+    let board = Board::default();
+    let interesting_board = Board::new([[0, 1, 2, 3], [4, 14, 6, 7], [0, 1, 2, 3], [4, 5, 6, 15]]);
     println!("{}", board);
+    println!("{}", interesting_board);
 }
