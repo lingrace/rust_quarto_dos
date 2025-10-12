31d33c9d78d63575081b41d90ae0c968daa60ea3
Grace Lin <lingrace@users.noreply.github.com>
2025-10-12 15:56:33 -0400

    pet pet: print board

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,22 @@
+// take input from user
+// handle help commands
+// display board
+// manage board data (including win conditions)
+
+ 
+const BOARD_SIZE: usize = 4;
+
+
 fn main() {
-    println!("Hello, world!");
+    println!("pet pet");
+
+    let board = [[-1; BOARD_SIZE]; BOARD_SIZE]; // TODO: cannot initialize to -1
+    for row in board {
+        let mut row_str: String = "".to_string();
+        for col in row {
+            row_str += col.to_string().as_str();
+        }
+        println!("{}", row_str);
+    }
+
 }
