5e3e2b203b086081b5cb40814fb11bc1f6d2086e
Grace Lin <lingrace@users.noreply.github.com>
2025-10-12 16:35:38 -0400

    ^_^: fmt golf

--- src/main.rs
+++ src/main.rs
@@ -5,39 +5,40 @@
 
 use std::fmt::Display;
 
- 
 const BOARD_SIZE: usize = 4;
 
-
 struct Board {
-    cells: [[i32; BOARD_SIZE]; BOARD_SIZE]
+    cells: [[i32; BOARD_SIZE]; BOARD_SIZE],
 }
 
 impl Board {
     fn new() -> Self {
         Board {
-            cells:  [[-1; BOARD_SIZE]; BOARD_SIZE]
+            cells: [[-1; BOARD_SIZE]; BOARD_SIZE],
         }
     }
 }
 
 impl Display for Board {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        let mut board_str: String = "".to_string();
-        for row in self.cells {
-            if board_str != "" {
-                board_str += "\n";
-                // TODO: add line separator
-            }
-            let row_str_vec: Vec<String> = row.iter().map(|x| x.to_string()).collect(); 
-            let row_str = row_str_vec.join(" | ");
-            board_str += &row_str;
-        }
+        let board_str = self
+            .cells
+            .iter()
+            .map(|row| {
+                row.iter()
+                    .map(|x| x.to_string())
+                    .collect::<Vec<_>>()
+                    .join(" | ")
+            })
+            .collect::<Vec<_>>()
+            .join("\n");
+        // TODO add line separator
+
         return write!(f, "{}", board_str);
     }
 }
 fn main() {
-    println!("grrr");
+    println!("^_^");
     let board = Board::new();
     println!("{}", board);
 }
